738c3f8fea4823225a42fc8acf6bf524390b46a3
Kyle Z Condie <barber.von.frankenshtien@gmail.com>
2021-01-15 22:27:17 -0700

    added styling. refactoring callbacks in to promises.

--- app.js
+++ app.js
@@ -133,13 +133,20 @@ const promptProjects = portfolioData => {
 promptUser()
 .then(promptProjects)
 .then(portfolioData => {
-    const pageHTML = generatePage(portfolioData);
-
-    fs.writeFile('./index.html', pageHTML, err => {
-        if (err) throw err;
-    
-        console.log('Portfolio complete! Check out index.html to see the output!');
-    });
+    return generatePage(portfolioData);
+})
+.then(pageHTML => {
+    return fs.writeFile(pageHTML);
+})
+.then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+})
+.then(copyFileResponse => {
+    console.log(copyFileResponse);
+})
+.catch(err => {
+    console.log(err);
 });
 
 
